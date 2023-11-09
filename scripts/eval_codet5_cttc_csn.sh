4636e777ce67ebc246d628c2a0127add6fe0aa70
Atharva Naik <atharvanaik2018@gmail.com>
2023-11-09 13:19:35 -0500

    pushing model outputs

--- scripts/eval_codet5_cttc_csn.sh
+++ scripts/eval_codet5_cttc_csn.sh
@@ -1,6 +1,6 @@
-SRC_LANG="php"
-TGT_LANG="en"
-BEST_CHECKPOINT_STEP="2500"
+SRC_LANG="en"
+TGT_LANG="ruby"
+BEST_CHECKPOINT_STEP="1500"
 TASK="code_x_glue_ct_code_to_text"
 python -m src.models.codet5 --mode eval \
 --output_dir "hf_out/codet5/codet5p_770m/${TASK}/${SRC_LANG}2${TGT_LANG}" \
